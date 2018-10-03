23eeb2c0704c5c9a9e64ce9c7071863e8ac8f270
snakeman118 <39317738+snakeman118@users.noreply.github.com>
2018-10-02 18:23:26 -0700

    done with part 2

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,12 +8,44 @@ public class Movement : MonoBehaviour
 {
     //set speed
     public float speed = 1.5f;
+    public bool set = false, done = false;
     //call for input of each key
     void Update()
     {
-
-        //left arrow moves to the left
-        if (Input.GetKey(KeyCode.LeftArrow))
+        //while shift is pressed move faster
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //set speed to double 
+            speed = 3.0f;
+        }
+        else
+        {
+            speed = 1.5f;
+        }
+        //when space is pressed reset
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //set player position to origional position
+            transform.position = new Vector3(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            set = !set;
+        }
+        if (set == true)
+        {
+            speed = 0f;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            done = true;
+        }
+        if (done == true)
+        {
+            speed = 0f;
+        }
+            //left arrow moves to the left
+            if (Input.GetKey(KeyCode.LeftArrow))
         {
             //set Z axis to ship looking left
             transform.eulerAngles = new Vector3(0, 0, 90);
